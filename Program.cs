1c54920ff6ba14154dda2ee043ea2668cea9382e
Darya <daryalas17@gmail.com>
2022-08-06 07:31:08 +0300

    seminar4_2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int t = int.Parse(Console.ReadLine());
+
+int getSum(int tLoc)
+{
+    int sum = 0;
+
+    while (tLoc > 0)
+    {
+        int temp = tLoc%10;
+        sum+= temp;
+        tLoc/= 10;
+
+    }
+
+    return sum;
+}
+
+Console.WriteLine(getSum(t));
